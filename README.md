3442abeafc0f1ac0ebfa9f32b21ac521e7541e7f
Francisco Puma <fppuma@gmail.com>
2023-03-16 21:20:09 -0500

    reverse function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # play-go
 Golang Playground
+
+Example based on this documentation
+```
+https://go.dev/doc/code
+```
+
--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"example/user/hello/morestrings"
+	"fmt"
+)
 
 func main() {
-  fmt.Println("Hello, world")
+	fmt.Println("Hello, world")
+	newMessage := morestrings.ReverseRunes("!oG, olleH")
+	fmt.Println(newMessage)
 }
